2cac2659ebba5bf5f61787a566d668ec064f4c2b
misilelaboratory <misileminecord@gmail.com>
2024-08-25 02:28:38 +0900

    feat: :recycle: ad-hoc client
    Signed-off-by: misilelaboratory <misileminecord@gmail.com>

--- client/main.py
+++ client/main.py
@@ -1,32 +1,64 @@
-import gradio as gr
+import sounddevice as sd
+from scipy.io.wavfile import write
 import numpy as np
-from pyannote.audio import Pipeline
+import threading
+from keyboard import add_hotkey, wait
+from loguru import logger
+from openai import OpenAI
 from tomli import loads
-from torch import tensor
+from httpx import post
+from playsound3 import playsound
 
 from pathlib import Path
 
-config = loads(Path("./config.toml").read_text())
+is_recording = False
+audio_data = []
+config = loads(Path("config.toml").read_text())
+o = OpenAI(api_key=config["token_openai"])
 
-pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-3.0", use_auth_token=config["auth_token"])
+def record_audio():
+  global is_recording, audio_data
+  while is_recording:
+    # Record audio in chunks
+    audio_chunk = sd.rec(int(44100 * 0.5), samplerate=44100, channels=1, dtype='int16')
+    sd.wait()
+    audio_data.append(audio_chunk) # type: ignore
 
-def transcribe(stream, new_chunk):
-    sr, y = new_chunk
-    y = y.astype(np.float32)
-    y /= np.max(np.abs(y))
+def start_recording():
+  global is_recording, audio_data
+  logger.info("rec start")
+  if not is_recording:
+    is_recording = True
+    audio_data = []
+    # deepcode ignore MissingAPI: <please specify a reason of ignoring this>
+    thread = threading.Thread(target=record_audio)
+    thread.start()
 
-    if stream is not None:
-        stream = np.concatenate([stream, y])
-    else:
-        stream = y
-    return stream, pipeline({"audio": stream})
+def stop_recording_and_save():
+  global is_recording, audio_data
+  if is_recording:
+    is_recording = False
+    logger.info("rec stop")
+    logger.debug("stage 1: save to wav")
+    # Concatenate all the audio chunks and save to WAV file
+    audio_data = np.concatenate(audio_data, axis=0)
+    write('output.wav', 44100, audio_data)
+    logger.debug("stage 2: send to whisper")
+    resp = o.audio.transcriptions.create(file=open('output.wav', 'rb'), model="whisper-1").text
+    logger.debug(f"resp: {resp}")
+    logger.debug("stage 3: send to butter")
+    resp = post(f"{config['BASE_URL']}/chat/send", timeout=None).content
+    Path("output_butter.wav").write_bytes(resp)
+    playsound("output_butter.wav", False)
+    audio_data = []
 
-demo = gr.Interface(
-    transcribe,
-    ["state", gr.Audio(sources=["microphone"], streaming=True)],
-    ["state", "text"],
-    live=True,
-)
+def main():
+  logger.debug("Hotkey pressed")
+  logger.debug(is_recording)
+  if not is_recording:
+    start_recording()
+  else:
+    stop_recording_and_save()
 
-demo.launch()
-
+add_hotkey('ctrl+q', main)
+wait()
