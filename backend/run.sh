48316ab7d945d6f868d19803974b33333537c02c
misilelab <misileminecord@gmail.com>
2024-09-25 12:31:52 +0900

    feat: add process-compose thing
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- backend/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export PATH="$PATH:$HOME/.local/bin"
-pdm install
-pdm run pip install torch torchaudio --index-url https://download.pytorch.org/whl/rocm6.1
-echo success
-pdm run fastapi run main.py --port 50000
