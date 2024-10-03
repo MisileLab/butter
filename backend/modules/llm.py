7f19d877ae6226b7b41f8055547c22f0096a7def
misilelaboratory <misileminecord@gmail.com>
2024-10-03 19:57:41 +0900

    some more vtube thing
    Signed-off-by: misilelaboratory <misileminecord@gmail.com>

--- backend/modules/llm.py
+++ backend/modules/llm.py
@@ -1,9 +1,9 @@
 from .config import api_key
-from . import memory, llm_function
+from . import memory, llm_function, vtube
 
 from langchain_openai import ChatOpenAI
 
-functions = {**llm_function.functions, **memory.functions}
-middle_converting_functions = {**llm_function.middle_converting_functions, **memory.middle_converting_functions}
+functions = {**llm_function.functions, **memory.functions, **vtube.functions}
+middle_converting_functions = {**llm_function.middle_converting_functions, **memory.middle_converting_functions, **vtube.middle_converting_functions}
 
 llm = ChatOpenAI(model="gpt-4o", api_key=api_key).bind_tools(list(functions.values()))
