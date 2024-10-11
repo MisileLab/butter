f2c8aa707a0f24433e7804f7feb7c5638f7ab67c
misilelaboratory <misileminecord@gmail.com>
2024-10-12 02:56:31 +0900

    fix: let's try this way+
    Signed-off-by: misilelaboratory <misileminecord@gmail.com>

--- backend/modules/llm.py
+++ backend/modules/llm.py
@@ -7,4 +7,4 @@ functions = {**llm_function.functions, **memory.functions}
 middle_converting_functions = {**llm_function.middle_converting_functions, **memory.middle_converting_functions}
 
 llm = ChatOpenAI(model="gpt-4o", api_key=api_key).bind_tools(list(functions.values()))
-llm_vtube = ChatOpenAI(model="gpt-4o", api_key=api_key).with_structured_output(vtube.VTubeModel, strict=True)
+llm_vtube = ChatOpenAI(model="gpt-4o", api_key=api_key).with_structured_output(vtube.VTubeModel, strict=True, method="json_schema")
--- backend/modules/vtube.py
+++ backend/modules/vtube.py
@@ -2,19 +2,19 @@ from pydantic import BaseModel, Field
 
 class FaceAngle(BaseModel):
   """Live2D model's face angle"""
-  x: float = Field(ge=-30,le=30)
-  y: float = Field(ge=-30,le=30)
-  z: float = Field(ge=-90,le=90)
+  x: float = Field(description="-30~30")
+  y: float = Field(description="-30~30")
+  z: float = Field(description="-90~90")
 
 class Eye(BaseModel):
   """Live2D model's eye"""
-  opened: float = Field(ge=0,le=1)
-  smiled: float = Field(ge=0,le=1)
+  opened: float = Field(description="0~1")
+  smiled: float = Field(description="0~1")
 
 class EyeBall(BaseModel):
   """Live2D model's eyeball"""
-  x: float = Field(ge=-1,le=1)
-  y: float = Field(ge=-1,le=1)
+  x: float = Field(description="-1~1")
+  y: float = Field(description="-1~1")
 
 class Point(BaseModel):
   "Point of bezier curve"
@@ -22,9 +22,9 @@ class Point(BaseModel):
   leftEye: Eye = Field(description="Left eye of Live2D model")
   rightEye: Eye = Field(description="Right eye of Live2D model")
   eyeBall: EyeBall = Field(description="Eye ball of Live2D model")
-  eyeBrow: float = Field(description="Eye brow of Live2D model", ge=-1, le=1)
+  eyeBrow: float = Field(description="Eye brow of Live2D model (-1~1)")
 
 class VTubeModel(BaseModel):
   "Move Live2D model"
   points: list[Point] = Field(description="List of points that will connecting by bezier curve")
-  second: float = Field(description="Duration of the movement", ge=0)
+  second: float = Field(description="Duration of the movement (>=0)")
